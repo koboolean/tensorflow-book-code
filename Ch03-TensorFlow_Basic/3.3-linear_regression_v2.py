dbaf0a24b0120ba97de7833717dde20b59a5a7d4
조현준 <45157196+koboolean@users.noreply.github.com>
2024-05-20 17:36:00 +0900

    머신러닝 기초 프로세스 - Linear Regression

--- Ch03-TensorFlow_Basic/3.3-linear_regression_v2.py
+++ Ch03-TensorFlow_Basic/3.3-linear_regression_v2.py
@@ -2,6 +2,9 @@
 
 import tensorflow as tf
 
+# 가설정의 함수를 정의
+# --------------------------------------------------
+
 # 선형회귀 모델(Wx + b)을 위한 tf.Variable을 선언합니다.
 W = tf.Variable(tf.random.normal(shape=[1]))
 b = tf.Variable(tf.random.normal(shape=[1]))
@@ -10,11 +13,15 @@ b = tf.Variable(tf.random.normal(shape=[1]))
 def linear_model(x):
   return W*x + b
 
+# --------------------------------------------------
+
 # 손실 함수를 정의합니다.
 # MSE 손실함수 \mean{(y' - y)^2}
 @tf.function
 def mse_loss(y_pred, y):
   return tf.reduce_mean(tf.square(y_pred - y))
+
+# --------------------------------------------------
 
 # 최적화를 위한 그라디언트 디센트 옵티마이저를 정의합니다.
 optimizer = tf.optimizers.SGD(0.01)
@@ -28,6 +35,10 @@ def train_step(x, y):
   gradients = tape.gradient(loss, [W, b])
   optimizer.apply_gradients(zip(gradients, [W, b]))
 
+# --------------------------------------------------
+
+# 실 수행이 수행되어 값을 출력되도록 해주는 파트
+
 # 트레이닝을 위한 입력값과 출력값을 준비합니다.
 x_train = [1, 2, 3, 4]
 y_train = [2, 4, 6, 8]
@@ -40,4 +51,5 @@ for i in range(1000):
 x_test = [3.5, 5, 5.5, 6]
 # 테스트 데이터를 이용해 학습된 선형회귀 모델이 데이터의 경향성(y=2x)을 잘 학습했는지 측정합니다.
 # 예상되는 참값 : [7, 10, 11, 12]
+# 실제 진행된 후의 참값 01) [ 6.9823813  9.935178  10.919444  11.903709 ]
 print(linear_model(x_test).numpy())
